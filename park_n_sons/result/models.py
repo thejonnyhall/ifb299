9e07a924f883de25f804ba1cfeb20d302f507689
Oswald Doring <oswald.doring98@hotmail.com>
2017-09-11 19:42:16 +1000

    results model

--- park_n_sons/result/models.py
+++ park_n_sons/result/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Results(models.Model):
+    name = models.CharField(max_length=20, default='NONE')
+    type_business = models.CharField(max_length=20, default='NONE')
+    address = models.CharField(max_length=50, default='NONE')
+    suburb = models.CharField(max_length=20, default='NONE')
+    lat_coord = models.CharField(max_length=10, default='NONE')
+    long_coord = models.CharField(max_length=10, default='NONE')
