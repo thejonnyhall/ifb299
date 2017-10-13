128c3e24b4aabca6c39ab4b9d1b771585eae3dd4
Jonny Hall <jonnyhall@bigpond.com>
2017-10-13 16:12:17 +1000

    Registration adds user to database
    Login it states that user and password don't match

--- park_n_sons/user/urls.py
+++ park_n_sons/user/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
-
+from django.conf import settings
+from django.contrib.auth import *
 from . import views
 
 urlpatterns = [
     url(r'^login/', views.login, name='login'),
     url(r'^register/', views.register, name='register'),
+    url(r'^logout/', views.logout, name='logout')
+
 ]
