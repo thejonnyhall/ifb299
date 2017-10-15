c09ca1364166ed5ce7a5dcf1cf2963ad7f14ab5f
Thungo <jonmarc.mail@gmail.com>
2017-10-15 15:52:43 +1000

    Functional results page

--- park_n_sons/result/views.py
+++ park_n_sons/result/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from search.forms import SearchForm
+from django.db.models import Q
+from functools import reduce
+import operator
+from .models import Results
 
 def result(request):
-    return render(request, "result.html", {})
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            words = data['keywords'].split(' ')
+            search = reduce(operator.or_, (Q(name__icontains=x) for x in words))
+            results = Results.objects.filter(search)
+            buildingList = ["College", "Library", "Industry", "Hotel", "Park", "Zoo", "Museum", "Restaurant", "Mall"]
+            
+            return render(request, "result.html", {'results': results, 'business': buildingList})
+        
+    return HttpResponseRedirect('/search/')
