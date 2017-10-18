6f9269b96a309733ca90652f45078a98300ec7c0
Thungo <jonmarc.mail@gmail.com>
2017-10-18 12:34:40 +1000

    Update looks

--- park_n_sons/search/forms.py
+++ park_n_sons/search/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    keywords = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Search'}))
+    keywords = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Search'}), required=False)
     free = forms.BooleanField(label='Free Activity', required=False)
     publicTransport = forms.BooleanField(label='Accessible by Public Transport', required=False)
     childFriendly = forms.BooleanField(label='Child Friendly', required=False)
