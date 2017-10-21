91b625c2937b5fdfc586d3e943fd9d9f19e19379
Thungo <jonmarc.mail@gmail.com>
2017-10-21 13:07:47 +1000

    Include Search on Results page and Search by User Type

--- park_n_sons/result/views.py
+++ park_n_sons/result/views.py
@@ -6,15 +6,44 @@ from django.db.models import Q
 from functools import reduce
 import operator
 from .models import Results
+from django.core.cache import cache
 
 def result(request):
-    form = SearchForm(request.POST)
-    if form.is_valid():
-        data = form.cleaned_data
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
 
-        words = data['keywords'].split(' ')
-        search = reduce(operator.or_, (Q(name__icontains=x) for x in words))
-        results = Results.objects.filter(search)
+            words = data['keywords'].split(' ')
+            userType = data['userType']
+            buildingFilter = []
+            if userType == 'bsns':
+                buildingFilter=[2, 3, 4, 5, 6, 7, 8]
+            elif userType == 'trst':
+                buildingFilter=[3, 4, 5, 6, 7, 8]
+            elif userType == 'sdnt':
+                buildingFilter=[0, 1, 4, 5, 6, 7, 8]
+            search = reduce(operator.or_, (Q(name__icontains=x) for x in words))
+            results = Results.objects.filter(search, type_business__in=buildingFilter)
+
+            buildingList = ["College", "Library", "Industry", "Hotel", "Park", "Zoo", "Museum", "Restaurant", "Mall"]
+
+            paginator = Paginator(results, 10)
+            page = request.GET.get('page')
+            try:
+                result_list = paginator.page(page)
+            except PageNotAnInteger:
+                result_list = paginator.page(1)
+            except EmptyPage:
+                result_list = paginator.page(paginator.num_pages)
+
+            searchForm = SearchForm()
+            cache.set('results', results)
+
+            return render(request, "result.html", {'results': result_list, 'business': buildingList, 'form': searchForm})
+    elif request.method == 'GET' and cache.get('results'):
+        results = cache.get('results')
+
         buildingList = ["College", "Library", "Industry", "Hotel", "Park", "Zoo", "Museum", "Restaurant", "Mall"]
 
         paginator = Paginator(results, 10)
@@ -26,6 +55,7 @@ def result(request):
         except EmptyPage:
             result_list = paginator.page(paginator.num_pages)
         
-        return render(request, "result.html", {'results': result_list, 'business': buildingList})
+        searchForm = SearchForm()
+        return render(request, "result.html", {'results': result_list, 'business': buildingList, 'form': searchForm})
         
     return HttpResponseRedirect('/search/')
--- park_n_sons/search/forms.py
+++ park_n_sons/search/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
+USERTYPE = (
+    ('bsns','Businessman'),
+    ('trst','Tourist'),
+    ('sdnt','Student'),
+)
+
 class SearchForm(forms.Form):
     keywords = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Search'}), required=False)
+    userType = forms.ChoiceField(label='', choices=USERTYPE)
     free = forms.BooleanField(label='Free Activity', required=False)
     publicTransport = forms.BooleanField(label='Accessible by Public Transport', required=False)
     childFriendly = forms.BooleanField(label='Child Friendly', required=False)
