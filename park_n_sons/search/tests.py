14e864c3112a5be92a460474999454fba7d19749
Oswald <oswald.doring98@hotmail.com>
2017-10-23 17:03:17 +1000

    search tests

--- park_n_sons/search/tests.py
+++ park_n_sons/search/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from search.forms import SearchForm
 # Create your tests here.
+
+class searchtests(TestCase):
+    def test_form(self):
+        form_data = {'userType':'Businessman'}
+        form = SearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
