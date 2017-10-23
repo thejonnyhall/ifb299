bc5a547c09a08db3b20434de6f863342fe0181c3
Oswald <oswald.doring98@hotmail.com>
2017-10-23 17:01:39 +1000

    updating user forms

--- park_n_sons/user/forms.py
+++ park_n_sons/user/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 import re
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.forms import UserCreationForm
 
-class RegistrationForm(forms.Form):
-    pass
+USERTYPE = (
+    ('bsns','Businessman'),
+    ('trst','Tourist'),
+    ('sdnt','Student'),
+)
+
+class UserCreateForm(UserCreationForm):
+    UserType = forms.ChoiceField(label='', choices=USERTYPE)
+
+    class Meta:
+        model = User
+        fields = ("username", "UserType","password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        if commit:
+            user.save()
+        return user
